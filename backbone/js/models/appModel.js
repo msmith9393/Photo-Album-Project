6e518bd2c0f3c98a59ed6d7e7372377fcf0d673b
Megan Smith <megan@tokaypress.com>
2016-03-30 22:09:26 -0700

    Fix Merge Conflicts

--- backbone/js/models/appModel.js
+++ backbone/js/models/appModel.js
@@ -12,18 +12,6 @@ var AppModel = Backbone.Model.extend({
       this.set('currentImage', image);
     }, this);
 
-<<<<<<< HEAD
-||||||| merged common ancestors
-    params.imageCollection.on('addImage', function(newImageCollection) {
-      // this.set('currentImage', image);
-      this.set('currentCollection', newImageCollection);
-    }, this);
-=======
-    params.imageCollection.on('add', function() {
-      // this.set('currentCollection', this.get('currentCollection'));
-      console.log('CURRENTCOLLECTION AFTER', this.get('currentCollection'))
-    }, this);
->>>>>>> 203898d4e10597a32d9d98e4315ead9bc6c0b782
   }
 
 });
